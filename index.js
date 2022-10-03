42c23b51375d9bb1c22475a80fcd5dc1579d90ae
Anish <113669810+radlysamar@users.noreply.github.com>
2022-10-03 09:31:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let name = "anish"
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[3])
+console.log(name[4])
+console.log(name.length)
+let devil = 'shubham'
+console.log(devil)
+
+//template literals
+let fr1 ="anish"
+let fr2 ="rahul"
+console.log(fr1, "is a friend of" ,fr2)
+//anish is friend of anish
+let sen = `${fr1} is a friend of ${fr2}`
+console.log(sen)
+
+//Escape sequence characters
+let veg ='carr\'ot'
+console.log(veg.length)
+let ve ="carr\"ot"
+console.log(ve)
+// let vege ="carr\not"
+// console.log(vege)
+// let veget ="ca\rrrot"
+// console.log(veget)
+let veget ="carr\tot"
+console.log(veget)
